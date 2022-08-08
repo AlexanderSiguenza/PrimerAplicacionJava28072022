2e977e3fa22a350957f4c4c17bb0c5424c5c5895
CARLA MARIA ARTIGA NAVARRETE <202000791@ujmd.edu.sv>
2022-08-08 15:10:12 -0600

    Creación rama Carla

--- src/primeraplicacionjava28072022/PrimerAplicacionJava28072022.java
+++ src/primeraplicacionjava28072022/PrimerAplicacionJava28072022.java
@@ -18,6 +18,7 @@ public class PrimerAplicacionJava28072022 {
         // TODO code application logic here
         System.out.println("hola mundo");
         System.out.println("hola alex");
+        System.out.println("Hola Carla");
     }
     
 }
